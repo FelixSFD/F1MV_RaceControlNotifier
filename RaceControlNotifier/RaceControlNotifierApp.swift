f6e3c11f04874fa8af648e7b9aa299ebe6248e28
FelixSFD <felixsfd@sobotics.org>
2023-05-28 18:53:07 +0200

    fix layout for iOS

--- RaceControlNotifier/RaceControlNotifierApp.swift
+++ RaceControlNotifier/RaceControlNotifierApp.swift
@@ -69,6 +69,7 @@ struct RaceControlNotifierApp: App {
                         Text("Settings")
                     }
             }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
         }
         
         #endif
